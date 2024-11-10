ba92758a5941392602faac87b20b7c3909eb38b4
CaoimheC131 <157409741+CaoimheC131@users.noreply.github.com>
2024-11-10 12:57:30 +0000

    Add files via upload

---
+++ src/main/java/org/example/caoimhespetitions/CaoimhespetitionsApplication.java
@@ -0,0 +1,13 @@
+package org.example.caoimhespetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaoimhespetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaoimhespetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/caoimhespetitions/CaoimhespetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.caoimhespetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaoimhespetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
